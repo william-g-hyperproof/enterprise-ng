d92d63fed4927a6535dc9acd88af73d16c3b253f
Bill Garrett <william.g@hyperproof.io>
2026-02-18 21:23:06 -0800

    Harpo: allow CORS

--- harpo/src/main/java/harpo/endpoints/HomeEndpoint.java
+++ harpo/src/main/java/harpo/endpoints/HomeEndpoint.java
@@ -1,12 +1,14 @@
 package harpo.endpoints;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import harpo.event.DemoEvent;
 import harpo.event.EventBus;
 
+@CrossOrigin
 @RestController
 public class HomeEndpoint {
   private EventBus<DemoEvent> demoEventBus;
